0baf7dc6eb412cbd67fb75385d35c68dcda3eca6
morphage32 <mwigand32@yahoo.com>
2023-07-23 22:52:47 -0700

    Created a loop to allow a user to start a new game once the current one is finished.

--- tictactoe.rb
+++ tictactoe.rb
@@ -142,4 +142,20 @@ def game()
 
 end
 
-game()
+playing = 'f'
+
+until playing == 'n'
+  game()
+  puts "Would you like to play again? 'y' for yes, 'n' for no"
+  playing = 'f'
+
+  until playing == 'y' || playing == 'n'
+    playing = gets.chomp
+    playing = playing.downcase
+    unless playing == 'y' || playing == 'n'
+      puts "Sorry, invalid entry. Please type 'y' for yes, 'n' for no."
+    end
+  end
+end
+
+puts "Bye!"

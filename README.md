5a3b132df2d4ab90585aa4910633907c015a11c4
morphage32 <mwigand32@yahoo.com>
2023-07-23 18:43:27 -0700

    Files added, function created to render gameboard in terminal. Other game functions skeletonized

---
+++ README.md
@@ -0,0 +1,3 @@
+Tic Tac Toe Ruby Project
+
+The final product of this project should be a functioning two-player tic-tac-toe game that is playable within a terminal window using Ruby. The project is meant to feature classes and methods in a practical use case.
---
+++ tictactoe.rb
@@ -0,0 +1,57 @@
+class Player
+  attr_reader :name, :letter
+
+  def initialize(name, letter)
+    @name = name
+    @letter = letter
+  end
+
+end
+
+class Game
+  
+  def initialize()
+    @game_board = Array.new(9)
+  end
+
+  def build_board()
+    puts #newline for new board
+    row = 1
+    slot = 0
+    5.times do
+      # check if this is a row with slots
+      if row.odd?
+        # print each slot, with a line between each
+        3.times do
+          if @game_board[slot] == nil
+            print slot + 1
+          else
+            print @game_board[slot]
+          end
+
+          unless (slot + 1) % 3 == 0
+            print '|'
+          end
+
+          slot += 1
+        end
+        row += 1
+      else
+        puts
+        print "-+-+-"
+        puts
+        row += 1
+      end
+    end
+    puts
+  end
+
+  def play_turn(player)
+
+  end
+
+
+end
+
+game1 = Game.new()
+game1.build_board

33f964cbfd1ad61c05914bfb3f107f116848713a
morphage32 <mwigand32@yahoo.com>
2023-08-20 00:54:16 -0700

    game function moved to main file so all objects can be created through the main file.

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,49 @@
-require './tictactoe.rb'
+require_relative 'board'
+require_relative 'player'
+
+def game()
+
+  player1 = Player.new("X")
+  player2 = Player.new("O")
+  board = Board.new()
+  turn = 1
+  victor = 'none'
+
+  if rand(1..2) == 1
+    current_player = player1
+  else
+    current_player = player2
+  end
+
+  while turn < 10 && victor == 'none'
+    board.build_board
+
+    if current_player == player2
+      current_player = player1
+    else
+      current_player = player2
+    end
+
+    current_player.turn_prompt()
+    board.update_board(current_player.name, current_player.letter)
+    victor = board.declare_winner()
+
+    turn += 1
+  end
+
+  board.build_board
+  puts
+
+  case victor
+    when 'X'
+      puts "#{player1.name} has won the game!"
+    when 'O'
+      puts "#{player2.name} has won the game!"
+    else
+      puts "The game has ended in a tie!"
+    end
+
+end
 
 playing = 'f'
 
